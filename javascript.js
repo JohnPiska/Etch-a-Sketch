f176b0e5020af0946686284661d832d3c51d4744
JohnPiska <159447800+JohnPiska@users.noreply.github.com>
2025-06-10 16:53:48 +0500

    change css grid to css flexbox start to make hover logic add a button

--- javascript.js
+++ javascript.js
@@ -1,8 +1,19 @@
 const container = document.querySelector(".grid-container");
+const button = document.querySelector(".btn");
+
+
+button.addEventListener("click", () => {
+    const size = prompt("Введите размер от 1 до 100");
+});
 
 for(let i = 0; i < 16 * 16; i++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("grid-item");
-    gridItem.textContent = i;
     container.appendChild(gridItem);
 }
+
+const gridItem = document.querySelector(".grid-item")
+gridItem.addEventListener("mouseover", () => {
+    gridItem.setAttribute("style", "background: blue;");
+    console.log("fdfsdfs")
+})
